97ec121011f5d87bfd100845e4bb06e2c57b91ac
GalloJunior <gallojunior@gmail.com>
2024-04-03 12:26:59 -0300

    Criação dos componentes de Seleção e Listagem de Emojis

--- StickerSmash/App.js
+++ StickerSmash/App.js
@@ -6,23 +6,31 @@ import Button from './src/components/Button';
 import CircleButton from './src/components/CircleButton';
 import IconButton from './src/components/IconButton';
 import * as ImagePicker from 'expo-image-picker';
+import EmojiPicker from './src/components/EmojiPicker';
+import EmojiList from './src/components/EmojiList';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [pickedEmoji, setPickedEmoji] = useState(null);
 
   const onReset = () => {
     setShowAppOptions(false);
   }
 
   const onAddSticker = () => {
-    //
+    setIsModalVisible(true);
   }
 
   const onSaveImageAsync = () => {
     //
+  }
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   }
 
   const pickImageAsync = async () => {
@@ -67,6 +75,9 @@ export default function App() {
           />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
       <StatusBar style="auto" />
     </View >
   );
